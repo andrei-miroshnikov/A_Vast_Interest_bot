dd932cd8cb7e1e70dd4c0731d9f70373230740f9
andrei-miroshnikov <77438816+andrei-miroshnikov@users.noreply.github.com>
2021-02-12 00:49:20 +0200

    Update bot_code.py

--- bot_code.py
+++ bot_code.py
@@ -3,13 +3,21 @@ from telebot import types
 import numpy as np
 bot = telebot.TeleBot('1605880250:AAFlVEe8CQMIuFY1rRYMcPtuW-UtYMlstjk')
 
-@bot.message_handler(commands=['start','go'])
-
-def bot_greeting(message):
-    if "привет" in message.text.lower() or "hello" in message.text.lower() or "здравствуй" in message.text.lower() or "hi" in message.text.lower():
-        bot.send_message(message.chat.id, 'Приветсвую тебя в лучшем бот-помощнике для лаборотории!')
-    else:
-        bot.send_message(message.chat.id, "Поприветствуй меня!😇")
 
 
-bot.polling()
+keyboard = telebot.types.ReplyKeyboardMarkup(False,True)
+keyboard.row('заглушка 1', 'заглушка 2', 'заглушка 3','заглушка 4')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+         bot.reply_to(message, f'Привет, {message.from_user.first_name}. Я бот. Чтобы начать, напиши \'Hello\' или \'Привет\'.')
+
+        
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    bot.send_message(message.chat.id, 'Ты попросил помощи, но помочь мне тебе нечем... вот тебе заглушка..', reply_markup=keyboard)
+
+    
+    
+   
+bot.polling(True)
